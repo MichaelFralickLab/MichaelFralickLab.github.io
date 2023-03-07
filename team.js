2c356ae82da2bf6ab8a4cb54f7fcd803df15e6ab
jmoggridge <ja.moggridge@gmail.com>
2023-03-07 04:20:50 -0800

    added social icons

--- team.js
+++ team.js
@@ -1,23 +1,84 @@
-// get DOM node for team member list
+function TwitterIcon(link) {
+  return `<a href=${link} class="team-card-icon">
+            <img
+                src='/images/icon/twitter.png'
+                height='30px'
+                style='fill: rgb(14, 123, 247); opacity: 50%'
+            />
+          </a>`;
+}
+function LinkedInIcon(link) {
+  return `<a href=${link} class="team-card-icon">
+            <img
+                src='/images/icon/linkedin.png'
+                height='30px'
+                style='fill: rgb(14, 123, 247); opacity: 50%'
+            />
+          </a>`;
+}
+function GithubIcon(link) {
+  return `<a href=${link} class="team-card-icon">
+            <img
+                src='/images/icon/github.png'
+                height='30px'
+                style='fill: rgb(14, 123, 247); opacity: 50%'
+            />
+          </a>`;
+}
+function WebIcon(link) {
+  return `<a href=${link} class="team-card-icon">
+            <img
+                src='/images/icon/website.png'
+                height='30px'
+                style='fill: rgb(14, 123, 247); opacity: 50%'
+            />
+          </a>`;
+}
+function DocIcon(link) {
+  return `<a href=${link} class="team-card-icon">
+            <img
+                src='/images/icon/publication.png'
+                height='30px'
+                style='fill: rgb(14, 123, 247); opacity: 50%'
+            />
+          </a>`;
+}
 
 // template for team member card
 function TeamMemberCard(member, index) {
-  let social = [];
-  //   if (member.social.keys().includes('twitter'))
-  //     social.push(
-  //       `<img
-  //               class='u-image u-image-default u-preserve-proportions u-image-10'
-  //               src='images/icon/twitter.png'
-  //               alt=''
-  //               data-image-width='128'
-  //               data-image-height='128'
-  //               data-href='https://twitter.com/johnlapp_'
-  //               data-target='_blank'
-  //             />`
-  //     );
+  let socialLinks = '';
+  if (member.social) {
+    const socialNames = Object.keys(member.social);
+    socialLinks = socialNames
+      .map((type) => {
+        let linkURL = member.social[type];
+        console.log('linkURL', linkURL);
+        let linkIcon;
+        switch (type) {
+          case 'twitter':
+            linkIcon = TwitterIcon(type);
+            break;
+          case 'github':
+            linkIcon = GithubIcon(type);
+            break;
+          case 'linkedin':
+            linkIcon = LinkedInIcon(type);
+            break;
+          case 'website':
+            linkIcon = WebIcon(type);
+            break;
+          default:
+            linkIcon = DocIcon(type);
+        }
+        return linkIcon;
+      })
+      .join();
+    console.log(socialLinks);
+  }
 
+  //* string-interpolate data into an html-template with styling from team.css
   return `
-          <div class="team-card" key=${index}>
+        <div class="team-card" key=${index}>
             <div
                 class="team-card-image"
                 style="background-image: url('images/team/${member.image}')"
@@ -28,8 +89,8 @@ function TeamMemberCard(member, index) {
                 ${member.name}
                 <span class="team-card-pronouns">(${member.pronouns})</span>
             </h5>
-            <p class="team-card-text u-text-variant">
-               <b> ${member.position}</b>
+            <p class="team-card-text team-card-position u-text-variant">
+                ${member.position}
             </p>
             <p class="team-card-text u-text-variant">
                 ${member.education}
@@ -39,19 +100,27 @@ function TeamMemberCard(member, index) {
                 ${member.aspirations}
                 <br />
             </p>
-          </div>
-          `;
+            <div class='team-card-links'>
+                ${socialLinks}
+            </div>
+        </div>
+        `;
 }
 
+//* get DOM node of container for team member list
 let usersContainer = document.getElementById('team-container');
 
-// read data from file asynchronously...
+//* Gets data, makes cards, adds to team page; script runs in team.html file
+// read data from file
 fetch('/public/team.json')
+  // wait for read to finish, then parse the file
   .then((response) => response.json())
   .then((data) => {
-    console.log('data', data);
+    // wait for, then use the data from the file
+    console.log('here is data', data);
+    // map team data to cards
     const cards = data.map((member, index) => TeamMemberCard(member, index));
-    console.log(cards);
+    // add js-generated cards into DOM
     usersContainer.innerHTML = cards;
     return;
   });
